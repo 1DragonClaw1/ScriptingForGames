d3ec627a34fa292387d9577469630e4f742115d2
Ella Weiss <1DragonClaw1@gmail.com>
2025-02-08 16:09:34 -0700

    FlipTransform
    Made the script FlipTransform to flip the sprite when the player presses the left or right arrow. Attached it to the Character and made sure it works well.

---
+++ AdventureGame/Class Project/Assets/Scripts/FlipTransform.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FlipTransform : MonoBehaviour
+{
+	public KeyCode key1 = KeyCode.RightArrow, key2 = KeyCode.LeftArrow;
+	public float direction1 = 0, direction2 = 180;    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key1))
+		{
+			transform.rotation = Quaternion.Euler(x:0,y:direction1,z:0);	
+		}
+		if (!Input.GetKeyDown(key2)) return;
+		transform.rotation = Quaternion.Euler(x:0,y:direction2,z:0);
+    }
+}
