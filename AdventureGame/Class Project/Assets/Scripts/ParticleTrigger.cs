5409d0a2550c21ac4a3c4f7ecc251fa67331b452
Ella Weiss <1DragonClaw1@gmail.com>
2025-03-30 21:50:47 -0600

    Updated the particle Trigger script
    I made it so that when the strawberry is touched, it will emit particles three times, the amount getting a little bigger each time.

--- AdventureGame/Class Project/Assets/Scripts/ParticleTrigger.cs
+++ AdventureGame/Class Project/Assets/Scripts/ParticleTrigger.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(ParticleSystem), typeof(Collider))]
 public class ParticleTrigger : MonoBehaviour
 {
     private ParticleSystem particleSystem; // Reference the particle system
-    public int particleAmount = 10; // Exposed variable for particle amount
+
+    public int firstEmissionAmount = 10; // Exposed variable for first emission
+    public int secondEmissionAmount = 20; // Exposed variable for second emission
+    public int thirdEmissionAmount = 30; // Exposed variable for third emission
+    public float delasyBetweenEmissions = 0.5f; // Delay time between the emissions
 
     private void Start()
     {
@@ -16,7 +21,21 @@ public class ParticleTrigger : MonoBehaviour
     {
         if (other.gameObject.GetComponent<CharacterController>()) // Check if player triggered the event
         {
-            particleSystem.Emit(particleAmount); //Emit the specified amount of particles
+            StartCoroutine(EmitParticlesCoroutine()); 
         }
+    }
+
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        // Frist emission
+        particleSystem.Emit(firstEmissionAmount);
+        yield return new WaitForSeconds(delasyBetweenEmissions);
+
+        // Second emission
+        particleSystem.Emit(secondEmissionAmount);
+        yield return new WaitForSeconds(delasyBetweenEmissions);
+
+        // Third emission
+        particleSystem.Emit(thirdEmissionAmount);
     }
 }
